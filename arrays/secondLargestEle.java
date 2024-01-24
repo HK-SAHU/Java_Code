94193f95d11e444fb98523b609a4eea9575aa9aa
HK-SAHU <135067768+HK-SAHU@users.noreply.github.com>
2024-01-24 15:25:57 +0530

    Add files via upload

--- arrays/secondLargestEle.java
+++ arrays/secondLargestEle.java
@@ -1,14 +1,29 @@
 public class secondLargestEle {
-    static void secLargetEle(int arr[]){
-        int max=arr[0];
-        for(int i=1; i<arr.length;i++){
+    static int LargetEle(int arr[]){
+        int max=Integer.MIN_VALUE;
+        for(int i=0; i<arr.length;i++){
                 if(arr[i]>max){
                     max=arr[i];
                 }
         }
+        return max;
+    }
+
+    static int secLargetEle(int[] arr){
+        int max= LargetEle(arr);
+        for(int i=0; i<arr.length;i++){
+            if(arr[i]==max){
+                arr[i]=Integer.MIN_VALUE;
+            }
+        }
+
+        int secMax= LargetEle(arr);
+        return secMax;
     }
     
     public static void main(String[] args) {
-        
+        // int arr[]={1,2,3,4,5,6,7,8,9};
+        int arr[]={-1,-2,-3,-4,-5,-6};
+        System.out.println(secLargetEle(arr)) ;
     }
 }
