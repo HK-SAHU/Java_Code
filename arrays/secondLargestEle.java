8217619ba12d654f548cfdd77038c2280487aa9e
HK-SAHU <135067768+HK-SAHU@users.noreply.github.com>
2024-01-24 16:48:16 +0530

    secondLargestEle.java

--- arrays/secondLargestEle.java
+++ arrays/secondLargestEle.java
@@ -1,25 +1,49 @@
 public class secondLargestEle {
-    static int LargetEle(int arr[]){
-        int max=Integer.MIN_VALUE;
-        for(int i=0; i<arr.length;i++){
-                if(arr[i]>max){
-                    max=arr[i];
-                }
+    static int LargestEle(int arr[]) {
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
         }
         return max;
+
     }
 
-    static int secLargetEle(int[] arr){
-        int max= LargetEle(arr);
-        for(int i=0; i<arr.length;i++){
-            if(arr[i]==max){
-                arr[i]=Integer.MIN_VALUE;
+    static int secLargetEle(int[] arr) {
+        int max = LargestEle(arr);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == max) {
+                arr[i] = Integer.MIN_VALUE;    // this will give us the minimum value.
             }
         }
 
-        int secMax= LargetEle(arr);
+        int secMax = LargestEle(arr);
         return secMax;
     }
+
+    
+    // static int LargetEle(int arr[]){
+    //     int max=Integer.MIN_VALUE;
+    //     for(int i=0; i<arr.length;i++){
+    //             if(arr[i]>max){
+    //                 max=arr[i];
+    //             }
+    //     }
+    //     return max;
+    // }
+
+    // static int secLargetEle(int[] arr){
+    //     int max= LargetEle(arr);
+    //     for(int i=0; i<arr.length;i++){
+    //         if(arr[i]==max){
+    //             arr[i]=Integer.MIN_VALUE;
+    //         }
+    //     }
+
+    //     int secMax= LargetEle(arr);
+    //     return secMax;
+    // }
     
     public static void main(String[] args) {
         // int arr[]={1,2,3,4,5,6,7,8,9};
