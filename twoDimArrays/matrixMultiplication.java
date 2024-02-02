ad23a0c32702dcff1f249843b0c8425a5eee83ba
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-02-02 15:44:35 +0530

    matrixMultiplication.java

--- twoDimArrays/matrixMultiplication.java
+++ twoDimArrays/matrixMultiplication.java
@@ -11,10 +11,34 @@ public class matrixMultiplication {
             }
         }
     }
+
+    static void printArray(int arr[][],int m,int c){
+        for(int i=0 ; i<m;i++){
+            for(int j=0; j<c; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
     
-    static void multiplication(int )
+    static void multiplication(int arr1[][], int arr2[][], int m ,int n, int r, int c){
+        if (m!=c){
+            System.out.println("not possible");
+            return;
+        }
+        int[][] ans = new int[m][c];
 
+        for(int i=0 ; i<m;i++){
+            for(int j =0 ; j<c;j++){
+                for(int k =0; k<n;k++){
+                    ans[i][j]+=arr1[i][k]*arr2[k][j];
+                }
+            }
+        }
 
+        printArray(ans, m, c);
+    }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("first array");
@@ -27,7 +51,6 @@ public class matrixMultiplication {
         int c= sc. nextInt();
         int [][] arr2= new int[r][c];
         takngInput(arr2, r, c);
-
-
+        multiplication(arr1, arr2, m,n,r, c);
     }
 }
