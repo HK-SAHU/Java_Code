df5fdd2cac3c231babbff725c7d55846f2c8177d
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-02-04 14:51:52 +0530

    Add files via upload

---
+++ twoDimArrays/rotateMatrix.java
@@ -0,0 +1,59 @@
+package twoDimArrays;
+
+import java.util.Scanner;
+
+public class rotateMatrix {
+
+    static void takngInput(int arr[][], int m, int n) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+    }
+
+    static void printArray(int arr[][], int m, int n) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void reverse(int arr[]) {
+        int i = 0;
+        int j = arr.length-1;
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    static void rotate(int arr[][], int m, int n) {
+        for (int i = 0; i < m; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            reverse(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] arr = new int[m][n];
+        takngInput(arr, m, n);
+        rotate(arr, m, n);
+        printArray(arr, m, n);
+    }
+}
