f9409fefcaabee14fe08788156a328d2d2b7f5d4
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-02-08 15:32:07 +0530

    Update reverseListMethod2.java

--- ArrayList/reverseListMethod2.java
+++ ArrayList/reverseListMethod2.java
@@ -1,7 +1,21 @@
 package ArrayList;
+import java.util.ArrayList;
 import java.util.Collections;
 
 public class reverseListMethod2 {
-   
+    public static void main(String[] args) {
+         ArrayList<Integer> l= new ArrayList<>();
+        l.add(0);
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add(4);
+        l.add(5);
+
+        Collections.reverse(l);
+
+        System.out.println(l);
+        // reverse(l);
+    }
     
 }
