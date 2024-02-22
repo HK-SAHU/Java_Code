98e268a90a523c5623b1a68f04c5dcf23ee7c45f
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-02-22 13:46:18 +0530

    thisKeyword.java

--- oops/Encapsulation/thisKeyword.java
+++ oops/Encapsulation/thisKeyword.java
@@ -2,17 +2,19 @@ class Student {
     private int age;
     private String Name;
     private String lastName;
+    public Object setData2;
 
     public void setData1(int age) {
-        age = age;  // local varibale >> instance variable  (SHADOWING PROBLEM)
+        this.age = age;  // local varibale >> instance variable  (SHADOWING PROBLEM)
     }
 
     public void setData4(int a){
         age=a;
     }
 
-    public void setData2() {
+    public String setData2() {
         Name = "Harsh";
+        return Name;
     }
 
     public void setData3(String lastName) {
@@ -20,7 +22,7 @@ class Student {
     }
 
     public void show() {
-        System.out.println(Name + " " + lastName + " " + age);
+        System.out.println(Name + " " + lastName + " " + age);   //all these are instance variable
     }
 
 }
@@ -30,18 +32,21 @@ public class thisKeyword {
 
         Student obj = new Student();
         Student obj1 = new Student();
-
+///////////////////// removing shadow probblem  this keyword ///////////////////////////////
         // obj.setData1(18);   //still the value will be null because local variable > instance variable
         // obj1.setData1(25);
-
+////////////////////// setData4 will show shadow problem /////////////////////////////////
         // obj.setData4(18);     // this will work as we are not giving the same variable name as the instance variable
         // obj1.setData4(25);
+////////////////////////////// calling setData3() method using object and printing it using show() method
+        // obj.setData3("Harsh");
+        // obj1.setData3("Anuj");
+        ////////////////////////////// to print the vlaue in setData2 method/////////////////////
+        // String name=obj.setData2();
+        // System.out.println(name);
+        //////////////////////////// calling show() method using object /////////////////////////
 
-        obj.setData3("Harsh");
-        obj1.setData3("Anuj");
-
-
-        obj1.show();
-        obj.show();
+        // obj1.show();
+        // obj.show();
     }
 }
